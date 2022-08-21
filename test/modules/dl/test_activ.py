d36072abcbe1714a431f0b1b6c097f69e3ddf167
Ilyas Mustafazade <mustafazadeilyas04@gmail.com>
2022-08-21 20:10:20 +0400

    Add test for identity activ.

---
+++ test/modules/dl/test_activ.py
@@ -0,0 +1,70 @@
+import unittest, sys, os
+import numpy as np
+from numpy.testing import assert_allclose
+
+
+FILE_DEPTH = 4
+sys.path.append(
+    "\\".join(os.path.abspath(__file__).split("\\")[:-FILE_DEPTH]))
+from modules.dl.activ import *
+
+
+class TestIdentity(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.mtx = np.array([[1, 2], [2, 3]])
+    
+    def test_identity(self):
+        act = identity(self.mtx)
+        des = self.mtx
+        assert_allclose(act, des, rtol=1e-8)
+    
+    def test_identity_deriv(self):
+        act = identity(self.mtx, deriv=True)
+        des = np.ones(self.mtx.shape)
+        assert_allclose(act, des, rtol=1e-8)
+    
+
+class TestReLU(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.mtx = np.array([[-12, 12, 23, -1],
+                            [-2, 11, 0, -12],
+                            [17, 19, 23, -33]])
+
+    def test_relu(self):
+        actual = relu(self.mtx, deriv=False)
+        desired = np.array([[0, 12, 23, 0],
+                            [0, 11, 0, 0],
+                            [17, 19, 23, 0]])
+        assert_allclose(actual, desired, rtol=1e-8)
+    
+    def test_relu_deriv(self):
+        actual = relu_deriv(self.mtx)
+        desired = np.array([[0, 1, 1, 0],
+                            [0, 1, 0, 0],
+                            [1, 1, 1, 0]])
+        assert_allclose(actual, desired, rtol=1e-8)
+        assert_allclose(relu(self.mtx, deriv=True), desired, rtol=1e-02)
+
+
+class TestSigmoid(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.mtx = np.array([[-1, 2], [3, -4]])
+
+    def test_sigmoid(self):
+        actual = sigmoid(self.mtx, deriv=False)
+        desired = np.array([[0.268941, 0.880797],
+                            [0.952574, 0.017986]])
+        assert_allclose(actual, desired, rtol=1e-02)
+    
+    def test_sigmoid_deriv(self):
+        actual = sigmoid_deriv(self.mtx)
+        desired = np.array([[0.196366, 0.104993],
+                            [0.045176, 0.017662]])
+        assert_allclose(actual, desired, rtol=1e-02)
+        assert_allclose(sigmoid(self.mtx, deriv=True), desired, rtol=1e-02)
+    
+
+if __name__ == "__main__": unittest.main(verbosity=3)
