e1aa3d27a92f583f7162f8ba42c29aad6dca09b2
Ilyas Mustafazade <mustafazadeilyas04@gmail.com>
2022-08-26 14:25:25 +0400

    Cosmetic changes

--- modules/dl/activ.py
+++ modules/dl/activ.py
@@ -1,35 +1,39 @@
 import numpy as np
 
 
-def identity(X, deriv=None):
-    if deriv is True: return identity_deriv(X)
-    return X
-
-def identity_deriv(X):
-    return np.ones(X.shape)
+def identity(x, deriv=False):
+    if deriv is True: return identity_deriv(x)
+    return x
 
 
-def relu(X, deriv=None):
-    if deriv is True: return relu_deriv(X)
-    return np.maximum(0, X)
-
-def relu_deriv(X):
-    return (X > 0).astype(X.dtype)
+def identity_deriv(x):
+    return np.ones(x.shape)
 
 
-def sigmoid(X, deriv=None):
-    if deriv is True: return sigmoid_deriv(X)
-    return 1 / (1 + np.exp(-X))
-
-def sigmoid_deriv(X):
-    sigmoid_x = sigmoid(X)
-    return np.multiply(sigmoid_x, (1 - sigmoid_x))
+def relu(x, deriv=False):
+    if deriv is True: return relu_deriv(x)
+    return np.maximum(0, x)
 
 
-def softmax(X, deriv=None):
-    if deriv is True: return softmax_deriv(X)  
-    exp_X = np.exp(X)
-    return exp_X / np.sum(exp_X, axis=1)
+def relu_deriv(x):
+    return (x > 0).astype(x.dtype)
 
-def softmax_deriv(X):
+
+def sigmoid(x, deriv=False):
+    if deriv is True: return sigmoid_deriv(x)
+    return 1 / (1 + np.exp(-x))
+
+
+def sigmoid_deriv(x):
+    sig_x = sigmoid(x)
+    return np.multiply(sig_x, (1 - sig_x))
+
+
+def softmax(x, deriv=False):
+    if deriv is True: return softmax_deriv(x)  
+    exp_x = np.exp(x)
+    return exp_x / np.sum(exp_x, axis=1)
+
+
+def softmax_deriv(x):
     raise NotImplementedError
