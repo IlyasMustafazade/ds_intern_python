7fa36721df2133c82c4f43c2cf36453d7f301845
Ilyas Mustafazade <mustafazadeilyas04@gmail.com>
2022-08-26 14:26:01 +0400

    Remove __str__()

--- modules/dl/layer.py
+++ modules/dl/layer.py
@@ -1,7 +1,8 @@
-import numpy as np, sys, os
+import numpy as np, sys, os, logging
 
 FILE_DEPTH = 3
-sys.path.append("\\".join(os.path.abspath(__file__).split("\\")[:-FILE_DEPTH]))
+sys.path.append(
+    "\\".join(os.path.abspath(__file__).split("\\")[:-FILE_DEPTH]))
 from modules.dl.activ import identity
 
 
@@ -9,71 +10,56 @@ class Layer:
     pass
 
 
-class InputLayer(Layer):
+class Input(Layer):
     def __init__(self, out_dim):
         self.out_dim = out_dim
-        self.activ_func = identity
+        self.activ_fn = identity
         self.output = None
         self.activ = None
 
-    def forward(self, in_mtx):
-        self.output = in_mtx
-        self.activ = self.output
+    def forward(self, in_mtx, save=True):
+        if save is True:
+            self.output = in_mtx
+            self.activ = self.output
         return in_mtx
 
 
-class DenseLayer(Layer):
-    def __init__(self, in_dim, out_dim, activ_func):
+class Dense(Layer):
+    def __init__(self, in_dim, out_dim, activ_fn):
         self.in_dim = in_dim
         self.out_dim = out_dim
         self.wgt = self.init_wgt()
         self.bias = self.init_bias()
         self.output = None
-        self.activ_func = activ_func
+        self.activ_fn = activ_fn
         self.activ = None
-        self.deriv_wrt_wgt = np.zeros(self.wgt.shape)
-        self.deriv_wrt_bias = np.zeros(self.bias.shape)
-
-    def __str__(self):
-        return f"\nNeuron count in previous layer -> {self.in_dim}\
-                 \nNeuron count in current layer -> {self.out_dim}\
-                 \nwgts -> \n{self.wgt}\
-                 \nBiases -> \n{self.bias}\
-                 \nactiv func -> \n{self.activ_func}\
-                 \nNet output -> \n{self.output}\
-                 \nactivs -> \n{self.activ}\
-                 \nPartial derivative w.r.t wgt mtx -> \n\
-                    {self.deriv_wrt_wgt}\
-                 \nPartial derivative w.r.t bias mtx -> \n\
-                    {self.deriv_wrt_bias}"
+        self.d_wgt = np.zeros(self.wgt.shape)
+        self.d_bias = np.zeros(self.bias.shape)
 
     def init_wgt(self):
-        wgt_shape = (self.in_dim, self.out_dim)
-        rand = np.random.rand(*wgt_shape)
-        rand = 2 * rand - 1
-        return rand
+        rand = np.random.rand(self.in_dim, self.out_dim)
+        return 2 * rand - 1
 
     def init_bias(self):
-        bias_shape = (self.out_dim, 1)
-        rand = np.random.rand(*bias_shape)
-        rand = 2 * rand - 1
-        return rand
+        rand = np.random.rand(self.out_dim, 1)
+        return 2 * rand - 1
 
-    def forward(self, in_mtx):
-        prod = self.wgt.T @ in_mtx
-        prod_with_bias = prod + self.bias
-        self.output = prod_with_bias
-        self.activ = self.activ_func(self.output)
-        return self.activ
-    
-    def set_deriv_wrt_wgt(self, X):
-        if X.shape != self.deriv_wrt_wgt.shape:
+    def forward(self, in_mtx, save=True):
+        net_output = self.wgt.T @ in_mtx + self.bias
+        activ = self.activ_fn(net_output)
+        if save is True:
+            self.output = net_output
+            self.activ = activ
+        return activ
+
+    def set_d_wgt(self, X):
+        if X.shape != self.d_wgt.shape:
             raise ValueError(
                 "Cost derivative w.r.t weight must be same shape as weight")
-        self.deriv_wrt_wgt = X
-    
-    def set_deriv_wrt_bias(self, X):
-        if X.shape != self.deriv_wrt_bias.shape:
+        self.d_wgt = X
+
+    def set_d_bias(self, X):
+        if X.shape != self.d_bias.shape:
             raise ValueError(
                 "Cost derivative w.r.t bias must be same shape as bias")
-        self.deriv_wrt_bias = X
+        self.d_bias = X
